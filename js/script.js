aa687f54280dc308823aec841aa315a5f73075d0
Ortello7 <domenicoortello00@gmail.com>
2024-12-13 19:38:22 +0100

    fixed responsive cards and sizes

--- js/script.js
+++ js/script.js
@@ -36,3 +36,25 @@ const teamMembers = [
     img: "img/female3.png"
   }
 ];
+
+
+// DOM elements
+const containerCard = document.querySelector(".container-cards");
+
+// Stampa delle card
+let listItem =""
+for (let i = 0; i < teamMembers.length; i++) {
+  const {name, role, email, img} = teamMembers[i];
+  listItem +=`
+  <div class="card-team bg-dark d-flex">
+  <div class="card-img">
+      <img src="./img/male1.png" alt="image">
+  </div>
+  <div class="card-info text-light me-5 p-2">
+      <h3>Marco Bianchi</h3>
+      <p>Designer</p>
+      <p id="mail_color">marcobianchi@team.com</p>
+  </div>
+</div>`
+}
+containerCard.innerHTML = listItem;
